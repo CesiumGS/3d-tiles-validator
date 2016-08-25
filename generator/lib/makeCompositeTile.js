5fb2426e2f399569f0304023915009a39f6cf591
Robert Taglang <minitags@comcast.net>
2016-08-25 11:37:04 -0400

    Aligned comments

--- generator/lib/makeCompositeTile.js
+++ generator/lib/makeCompositeTile.js
@@ -20,13 +20,13 @@ function makeCompositeTile(tileBuffers) {
         if (tilePadding !== 0) {
             tile = Buffer.concat([tile, new Buffer(4 - tilePadding)]);
         }
-        tile.writeUInt32LE(tile.length, 8); // byteLength
+        tile.writeUInt32LE(tile.length, 8);       // byteLength
         byteLength += tile.length;
         buffers.push(tile);
     }
-    header.write('cmpt', 0);                // magic
-    header.writeUInt32LE(1, 4);             // version
-    header.writeUInt32LE(byteLength, 8);    // byteLength
+    header.write('cmpt', 0);                      // magic
+    header.writeUInt32LE(1, 4);                   // version
+    header.writeUInt32LE(byteLength, 8);          // byteLength
     header.writeUInt32LE(tileBuffers.length, 12); // tilesLength
     return Buffer.concat(buffers);
 }
