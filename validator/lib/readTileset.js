87d932921c7f36187d9e11c82378ac00a970d81f
Sumit <ssumit@seas.upenn.edu>
2016-10-06 03:23:27 -0400

    added check for gzip

--- validator/lib/readTileset.js
+++ validator/lib/readTileset.js
@@ -1,15 +1,24 @@
 'use strict';
 var fs = require('fs-extra');
 var Promise = require('bluebird');
+var zlib = require('zlib');
+var isGzipped = require('./isGzipped');
 
-var fsReadFile = Promise.promisify(fs.readJson);
+var fsReadFile = Promise.promisify(fs.readFile);
+var zlibGunzip = Promise.promisify(zlib.gunzip);
+var parseJson = Promise.promisify(JSON.parse)
+
 
 function readTileset(filePath) {
     return fsReadFile(filePath)
-        .then(function (jsonObject) {
-            return jsonObject;
-        })
-        .catch(function(error)  {
-            throw error;
+        .then(function (buffer) {
+            if (isGzipped(buffer)) {
+                return zlibGunzip(buffer)
+                    .then(function(data) {
+                        return parseJson(data.toString());
+                })
+            } else {
+                return parseJson(buffer.toString());
+            }
         });
 }
