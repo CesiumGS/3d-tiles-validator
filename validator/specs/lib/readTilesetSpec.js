213c973739c449b8b464dc362feb493e318dac9d
Sumit <ssumit@seas.upenn.edu>
2016-10-13 15:11:58 -0400

    using defined for test

--- validator/specs/lib/readTilesetSpec.js
+++ validator/specs/lib/readTilesetSpec.js
@@ -1,18 +1,29 @@
 'use strict';
+
+var Cesium = require('cesium');
 var readTileset = require('../../lib/readTileset');
+var defined = Cesium.defined;
 
 describe('readTileset', function() {
     it('reads a tileset', function(done) {
         expect(readTileset('./specs/data/Tileset/tileset.json')
             .then(function(tilesetData) {
-                return (tilesetData['asset'] !== undefined)
+                if (defined(tilesetData['asset'])) {
+                    done();
+                } else {
+                    throwError("not a tileset");
+                }
             }), done).toResolve();
     });
 
     it('reads a gzipped tileset', function(done) {
         expect(readTile('./specs/data/TilesetGzipped/tileset.json')
             .then(function(tilesetData) {
-                return (tilesetData['asset'] !== undefined)
+                 if (defined(tilesetData['asset'])) {
+                     done();
+                 } else {
+                     throwError("not a tileset");
+                 }
             }), done).toResolve();
     });
 
