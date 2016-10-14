ee7e98ffbc839d6a037c69834daf8d5378c558c5
Sumit <ssumit@seas.upenn.edu>
2016-10-13 20:15:18 -0400

    removed Cesium defined

--- validator/specs/lib/readTilesetSpec.js
+++ validator/specs/lib/readTilesetSpec.js
@@ -1,29 +1,19 @@
 'use strict';
 
-var Cesium = require('cesium');
 var readTileset = require('../../lib/readTileset');
-var defined = Cesium.defined;
 
 describe('readTileset', function() {
     it('reads a tileset', function(done) {
         expect(readTileset('./specs/data/Tileset/tileset.json')
             .then(function(tilesetData) {
-                if (defined(tilesetData['asset'])) {
-                    done();
-                } else {
-                    throwError("not a tileset");
-                }
+                expect(tilesetData).toBeDefined();
             }), done).toResolve();
     });
 
     it('reads a gzipped tileset', function(done) {
         expect(readTile('./specs/data/TilesetGzipped/tileset.json')
             .then(function(tilesetData) {
-                 if (defined(tilesetData['asset'])) {
-                     done();
-                 } else {
-                     throwError("not a tileset");
-                 }
+                 expect(tilesetData).toBeDefined();
             }), done).toResolve();
     });
 
