505ae55d9e110737981c0d9333140a52bec98438
Sean Lilley <lilleyse@gmail.com>
2016-10-17 10:09:17 -0400

    Fix in readTileSpec

--- validator/specs/lib/readTileSpec.js
+++ validator/specs/lib/readTileSpec.js
@@ -11,7 +11,7 @@ describe('readTile', function() {
     });
 
     it('reads a gzipped tile', function(done) {
-        expect(readTile('./specs/data/Tileset/parent.b3dm')
+        expect(readTile('./specs/data/TilesetGzipped/parent.b3dm')
             .then(function(tileData) {
                 var magic = tileData.toString('utf8', 0, 4);
                 expect(magic).toEqual('b3dm');
