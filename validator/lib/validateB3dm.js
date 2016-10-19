978cedfdd173147dcaf2c2d2c36cf393be230eff
Judy Weng <judyweng@seas.upenn.edu>
2016-10-18 23:09:30 -0400

    trying to start validatorB3dmSpec, but a bit confused

--- validator/lib/validateB3dm.js
+++ validator/lib/validateB3dm.js
@@ -1,26 +1,36 @@
 'use strict';
-
 var Cesium = require('cesium');
+
 var DeveloperError = Cesium.DeveloperError;
+
 module.exports = validateB3dm;
 
 /**
- * Test if the provided data is gzipped.
+ * Checks if provided buffer has valid b3dm tile contents
  *
- * @param {Buffer} content A buffer containing the contents of a b3dm tileset.
- * @returns {Boolean} True if the content is valid according to the specs:
- * https://github.com/AnalyticalGraphicsInc/3d-tiles/tree/master/TileFormats/Batched3DModel, False if not.
+ * @param {Buffer} content A buffer containing the contents of a b3dm tile.
+ * @returns {Boolean} True if the content is valid according to the spec:
+ * {@link https://github.com/AnalyticalGraphicsInc/3d-tiles/tree/master/TileFormats/Batched3DModel}, False if not.
  */
 function validateB3dm(content) {
-    if(content === undefined || content === null) {
-        throw new DeveloperError('b3dm content is undefined or null');
+    if(!Cesium.defined(content)) {
+        throw new DeveloperError('b3dm content must be defined');
     }
-    //buf.writeUInt32LE(value, offset[, noAssert])
-    //buf.readUInt32LE(offset[, noAssert])
-
     var magic = content.toString('uft8', 0, 4);
     var version = content.readUInt32LE(4);
     var byteLength = content.readUInt32LE(8);
 
-    return (magic === 'bd3m' && version === 0x01 && byteLength === content.length);
+    if(magic !== 'bd3m') {
+        return false;
+    }
+
+    if(version !== 1) {
+        return false;
+    }
+
+    if (byteLength !== content.length) {
+        return false;
+    }
+
+    return true;
 }
