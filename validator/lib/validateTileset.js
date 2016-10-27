2c7655fe82cdb36b38064ac0715bfd7cd9c50131
Sumit <ssumit@seas.upenn.edu>
2016-10-27 13:11:13 -0400

    addressed comments on PR

--- validator/lib/validateTileset.js
+++ validator/lib/validateTileset.js
@@ -4,36 +4,32 @@ var Promise = require('bluebird');
 module.exports = validateTileset;
 
 /**
- * Checks whether the geometricError of each tile is <= the geometric error of its parent tile.
+ * Walks down the tree represented by the JSON object and checks if it is a valid tileset.
  *
- * @param {JSON} The JSON object representing the tileset.
+ * @param {Object} tileset The JSON object representing the tileset.
  * @return {Promise} A promise that resolves with two parameters - (1) a boolean for whether the tileset is valid
  *                                                                 (2) the error message if the tileset is not valid.
  *
  */
 function validateTileset(tileset) {
     if (tileset.root.geometricError > tileset.geometricError) {
-        return Promise.resolve(false, "Root has geometricError greater than tileset");
+        return Promise.resolve(false, 'Root has geometricError greater than tileset');
     }
 
-    return new Promise(function(resolve, reject) {
-        checkGeometricError(tileset.root, tileset.geometricError, resolve);
+    return new Promise(function(resolve) {
+        validateNode(tileset.root, tileset.geometricError, resolve);
     });
 
 }
 
-function checkGeometricError(root, testError, resolve) {
-    // If we have reached a leaf, stop
-    if (root === undefined) {
-        return;
+function validateNode(node, parentGeometricError, resolve) {
+    if (node.geometricError > parentGeometricError) {
+        return resolve(false, 'Child has geometricError greater than parent');
     }
-    // Condition for invalid tileset
-    if (root.geometricError > testError) {
-        return resolve(false, "Child has geometricError greater than parent");
-    } else {
-        // Call the function with each child
-        for (var child in root.children) {
-            checkGeometricError(child, root.testError);
-        }
+    // Call the function with each child
+    var length = node.children.length;
+    for (var i = 0; i < length; i++) {
+            validateNode(node.children[i], node.geometricError, resolve);
     }
+
 }
--- validator/specs/lib/validateTilesetSpec.js
+++ validator/specs/lib/validateTilesetSpec.js
@@ -1,56 +1,57 @@
 'use strict';
 
-var validateTileset = require('../../lib/validateTileset');
 var clone = require('clone');
+var validateTileset = require('../../lib/validateTileset');
 
 describe('validateTileset', function() {
+    var tileset = {
+        "asset": {
+            "version": "0.0"
+        },
+        "geometricError": 240,
+        "root": {
+            "boundingVolume": {
+                "region": [-1.3197209591796106, 0.6988424218, -1.3196390408203893, 0.6989055782, 0, 88]
+            },
+            "geometricError": 70,
+            "refine": "add",
+            "children": [
+                {
+                    "boundingVolume": {
+                        "region": [-1.3197209591796106, 0.6988424218, -1.31968, 0.698874, 0, 20]
+                    },
+                    "geometricError": 50,
+                    "content": {
+                        "url": "a.b3dm"
+                    },
+                    "children": [
+                        {
+                            "boundingVolume": {
+                                "region": [-1.3197209591796106, 0.6988424218, -1.31968, 0.698874, 0, 10]
+                            },
+                            "geometricError": 0,
+                            "content": {
+                                "url": "b.b3dm"
+                            }
+                        }
+                    ]
+                },
+                {
+                    "boundingVolume": {
+                        "region": [-1.31968, 0.6988424218, -1.3196390408203893, 0.698874, 0, 20]
+                    },
+                    "geometricError": 0,
+                    "content": {
+                        "url": "c.b3dm"
+                    }
+                }
+            ]
+        }
+    };
 
     it('reads a valid JSON', function(done) {
-        var tileset = {
-            "asset": {
-                "version": "0.0"
-            },
-            "geometricError": 240,
-            "root": {
-                "boundingVolume": {
-                    "region": [-1.3197209591796106, 0.6988424218, -1.3196390408203893, 0.6989055782, 0, 88]
-                },
-                "geometricError": 70,
-                "refine": "add",
-                "children": [
-                    {
-                        "boundingVolume": {
-                            "region": [-1.3197209591796106, 0.6988424218, -1.31968, 0.698874, 0, 20]
-                        },
-                        "geometricError": 50,
-                        "content": {
-                            "url": "a.b3dm"
-                        },
-                        "children": [
-                            {
-                                "boundingVolume": {
-                                    "region": [-1.3197209591796106, 0.6988424218, -1.31968, 0.698874, 0, 10]
-                                },
-                                "geometricError": 0,
-                                "content": {
-                                    "url": "b.b3dm"
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        "boundingVolume": {
-                            "region": [-1.31968, 0.6988424218, -1.3196390408203893, 0.698874, 0, 20]
-                        },
-                        "geometricError": 0,
-                        "content": {
-                            "url": "c.b3dm"
-                        }
-                    }
-                ]
-            }
-        };
-        expect(validateTileset(tileset)).then(function(data) {
+        expect(validateTileset(tileset))
+            .then(function(data) {
            expect(data[0]).toBeTruthy();
         });
     });
