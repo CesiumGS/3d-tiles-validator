c9764142f4e03f654b6adfae2e96b2d9c7c315d9
Sumit <ssumit@seas.upenn.edu>
2016-10-31 15:53:47 -0400

    added command line integration

--- validator/bin/3d-tiles-validator.js
+++ validator/bin/3d-tiles-validator.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
 'use strict';
+
+var argv = require('yargs').argv;
+var fsExtra = require('fs-extra');
+var path = require('path');
+var Promise = require('bluebird');
+
+var readTileset = require('../lib/readTileset');
+var validateTileset = require('../lib/validateTileset');
+
+var filePath = argv._[0];
+var jsonTileset = readTileset(filePath);
+var response = validateTileset(jsonTileset);
+console.log(response);
