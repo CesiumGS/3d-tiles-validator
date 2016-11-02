bea2c3cdf38982ea7f4f751673a98d22730d45c8
Judy Weng <judyweng@seas.upenn.edu>
2016-11-02 02:27:42 -0400

    style fixes

--- validator/lib/validateB3dm.js
+++ validator/lib/validateB3dm.js
@@ -9,7 +9,7 @@ module.exports = validateB3dm;
 /**
  * Checks if provided buffer has valid b3dm tile content
  *
- * @param {Buffer} content A buffer containing the contents of a b3dm tile.
+ * @param {Buffer} content - A buffer containing the contents of a b3dm tile.
  * @returns {Object} An object with two parameters - (1) a boolean for whether the tile is a valid b3dm tile
  *                                                   (2) a message to indicate which tile field is invalid, if any
  */
