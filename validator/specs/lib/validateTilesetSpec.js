ce0009a35110822439f0828c2e6306e56aeb389e
Sumit <ssumit@seas.upenn.edu>
2016-11-21 09:20:22 -0500

    added region inside region test

--- validator/specs/lib/validateTilesetSpec.js
+++ validator/specs/lib/validateTilesetSpec.js
@@ -2,6 +2,7 @@
 
 var clone = require('clone');
 var validateTileset = require('../../lib/validateTileset');
+var validateBoundingVolume = require('../../lib/validateBoundingVolume');
 
 describe('validateTileset', function() {
     var tileset = {
@@ -76,4 +77,61 @@ describe('validateTileset', function() {
                 expect(response.message).toBe('Child has geometricError greater than parent');
             }), done).toResolve();
     });
+});
+
+
+describe('validateBoundingVolume', function() {
+   var tileset = {
+       "asset": {
+           "version": "0.0"
+       },
+       "geometricError": 240,
+       "root": {
+           "boundingVolume": {
+               "region": [-1.3197209591796106, 0.6988424218, -1.3196390408203893, 0.6989055782, 0, 88]
+           },
+           "geometricError": 70,
+           "refine": "add",
+           "children": [
+               {
+                   "boundingVolume": {
+                       "region": [-1.3197209591796106, 0.6988424218, -1.31968, 0.698874, 0, 20]
+                   },
+                   "geometricError": 50,
+                   "content": {
+                       "url": "a.b3dm"
+                   },
+                   "children": [
+                       {
+                           "boundingVolume": {
+                               "region": [-1.3197209591796106, 0.6988424218, -1.31968, 0.698874, 0, 10]
+                           },
+                           "geometricError": 0,
+                           "content": {
+                               "url": "b.b3dm"
+                           }
+                       }
+                   ]
+               },
+               {
+                   "boundingVolume": {
+                       "region": [-1.31968, 0.6988424218, -1.3196390408203893, 0.698874, 0, 20]
+                   },
+                   "geometricError": 0,
+                   "content": {
+                       "url": "c.b3dm"
+                   }
+               }
+           ]
+       }
+   };
+
+    it('validates region inside region', function(done) {
+        expect(validateBoundingVolume(tileset)
+            .then(function(response) {
+                expect(response.result).toBe(true);
+                expect(response.message).toBe('Tileset is valid');
+            }), done).toResolve();
+    });
+
 });
