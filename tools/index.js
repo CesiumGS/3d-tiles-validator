c2a1e75a07a7cbefd6cf36fb0357cd0dbcd59236
Shehzan Mohammed <zmohammed@agi.com>
2017-01-19 15:57:31 -0500

    Add glbToI3dm to index.js

--- tools/index.js
+++ tools/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     glbToB3dm : require('./lib/glbToB3dm'),
+    glbToI3dm : require('./lib/glbToI3dm'),
     gzipTileset : require('./lib/gzipTileset'),
     pipeline : require('./lib/runPipeline')
 };
