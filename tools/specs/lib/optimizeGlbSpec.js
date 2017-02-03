1108c7a375d9c10f61382e648109ccf670cae7fb
Shehzan Mohammed <zmohammed@agi.com>
2017-02-03 17:13:53 -0500

    Add spec and readme example for using compression with optimizeGlb

--- tools/specs/lib/optimizeGlbSpec.js
+++ tools/specs/lib/optimizeGlbSpec.js
@@ -24,6 +24,37 @@ describe('optimizeGlb', function() {
             }), done).toResolve();
     });
 
+    it('optimizes and compresses a glb using the gltf-pipeline', function(done) {
+        var compressionOptions = { aoOptions: undefined,
+            binary: false,
+            compressTextureCoordinates: false,
+            embed: true,
+            embedImage: true,
+            encodeNormals: false,
+            faceNormals: false,
+            tangentsBitangents: false,
+            stats: false,
+            inputPath: 'null',
+            kmcOptions: undefined,
+            optimizeForCesium: false,
+            outputPath: 'null-optimized.gltf',
+            preserve: false,
+            quantize: false,
+            removeNormals: false,
+            textureCompressionOptions: [ { quality: 10, format: 'dxt1' } ]
+        };
+
+        var promises = [];
+        promises.push(optimizeGlb(buffer));
+        promises.push(optimizeGlb(buffer, compressionOptions));
+
+        expect(Promise.all(promises)
+            .then(function(optimizedGlbs) {
+                expect(optimizedGlbs.length).toEqual(2);
+                expect(optimizedGlbs[0]).not.toEqual(optimizedGlbs[1]);
+            }), done).toResolve();
+    });
+
     it('throws an error if no buffer is provided', function() {
         expect(function() {
             optimizeGlb();
