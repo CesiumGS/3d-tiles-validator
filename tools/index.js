e6df6955617ea9b5f22c4ad9783fe740d633dc0f
Shehzan Mohammed <zmohammed@agi.com>
2017-02-24 15:03:18 -0500

    Add tools to index.js

--- tools/index.js
+++ tools/index.js
@@ -1,6 +1,11 @@
 module.exports = {
+    extractB3dm : require('./lib/extractB3dm'),
+    extractCmpt : require('./lib/extractCmpt'),
+    extractI3dm : require('./lib/extractI3dm'),
     glbToB3dm : require('./lib/glbToB3dm'),
     glbToI3dm : require('./lib/glbToI3dm'),
     gzipTileset : require('./lib/gzipTileset'),
-    pipeline : require('./lib/runPipeline')
+    optimizeGlb : require('./lib/gzipTileset'),
+    pipeline : require('./lib/runPipeline'),
+    tileset2sqlite3 : require('./lib/tileset2sqlite3')
 };
