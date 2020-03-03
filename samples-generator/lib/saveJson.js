f22793ff3b4746a1c15418a3edc961a31c618c20
Samuel Vargas <sam.vargas@cesium.com>
2020-03-03 09:39:21 -0500

    Add support for gzipping json in saveJson

--- samples-generator/lib/saveJson.js
+++ samples-generator/lib/saveJson.js
@@ -1,6 +1,8 @@
 'use strict';
 var Cesium = require('cesium');
 var fsExtra = require('fs-extra');
+var zlib = require('zlib');
+var saveBinary = require('./saveBinary');
 
 var defaultValue = Cesium.defaultValue;
 
@@ -12,14 +14,20 @@ module.exports = saveJson;
  * @param {String} path The path to save the tileset.json.
  * @param {Object} json The JSON.
  * @param {Boolean} [prettyJson=true] Whether to prettify the JSON.
+ * @param {Boolean} [useGzip=false] Whether to use GZIP or not
  *
  * @returns {Promise} A promise that resolves when the tileset.json is saved.
  */
-function saveJson(path, json, prettyJson) {
+function saveJson(path, json, prettyJson, gzip) {
     prettyJson = defaultValue(prettyJson, true);
     var options = {};
     if (prettyJson) {
         options.spaces = 2;
     }
+    gzip = defaultValue(gzip, false);
+    if (gzip) {
+        return saveBinary(path, json, gzip);
+    }
+
     return fsExtra.outputJson(path, json, options);
 }
