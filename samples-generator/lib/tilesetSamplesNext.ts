0e9b56d95564638fed0b2099e4e28ba97618d245
Samuel Vargas <sam.vargas@cesium.com>
2020-03-31 14:33:58 -0400

    TilesetSamplesNext → createTilesetEmptyRoot

--- samples-generator/lib/tilesetSamplesNext.ts
+++ samples-generator/lib/tilesetSamplesNext.ts
@@ -14,7 +14,8 @@ import {
     llTileOptions,
     lrTileOptions,
     urTileOptions,
-    ulTileOptions
+    ulTileOptions,
+    childrenRegion
 } from './constants';
 import { FeatureTableUtils } from './featureMetatableUtilsNext';
 import { Mesh } from './Mesh';
@@ -162,10 +163,7 @@ export namespace TilesetSamplesNext {
         };
 
         const fullPath = path.join(rootDir, 'Tileset')
-
-        // TODO: Fix types so any is unnecessary.
         await writeTileset(fullPath, tilesetJson as any, args);
-
         for (let i=0; i < gltfs.length; ++i) {
             const gltf = gltfs[i];
             const tileFilename = tileNames[i] + ext;
@@ -173,7 +171,133 @@ export namespace TilesetSamplesNext {
         }
     }
 
-    export async function createTilesetEmptyRoot(args: GeneratorArgs) {}
+    export async function createTilesetEmptyRoot(args: GeneratorArgs) {
+        const tileOptions = [
+            llTileOptions,
+            lrTileOptions,
+            urTileOptions,
+            ulTileOptions
+        ];
+
+        const tileNames = [
+            'll',
+            'lr',
+            'ur',
+            'ul'
+        ];
+
+        const buildings = tileOptions.map(opt =>
+            createBuildings(opt.buildingOptions)
+        );
+
+        const batchTables = buildings.map(building =>
+            generateBuildingBatchTable(building)
+        );
+
+        const batchedMeshes = buildings.map((building, i) => {
+            const transform = tileOptions[i].transform;
+            return Mesh.batch(
+                FeatureTableUtils.createMeshes(transform, building, false)
+            );
+        });
+
+        // remove explicit ID from batchTable, it's not used in the
+        // feature metadata gltf extension
+        batchTables.forEach(table => delete table.id);
+
+        const gltfs = batchedMeshes.map(mesh =>
+            createGltf({ mesh: mesh, useBatchIds: false })
+        ) as Gltf[];
+
+        gltfs.forEach((gltf, i) => {
+            const batchTable = batchTables[i];
+
+            FeatureMetadata.updateExtensionUsed(gltf);
+            FeatureMetadata.addFeatureTable(gltf, {
+                featureCount: batchTable.Longitude.length,
+                properties: {
+                    Longitude: { values: batchTable.Longitude },
+                    Latitude: { values: batchTable.Latitude },
+                    Height: { values: batchTable.Height }
+                }
+            });
+
+            const primitive = gltf.meshes[0].primitives[0];
+            FeatureMetadata.addFeatureLayer(primitive, {
+                featureTable: 0,
+                vertexAttribute: {
+                    implicit: {
+                        increment: 1,
+                        start: 0
+                    }
+                }
+            });
+        });
+
+
+        const ext = args.useGlb ? '.glb' : '.gltf';
+
+        var tilesetJson = {
+            asset : {
+                version : tilesNextTilesetJsonVersion
+            },
+            properties : undefined,
+            geometricError : smallGeometricError,
+            root : {
+                boundingVolume : {
+                    region : childrenRegion
+                },
+                geometricError : smallGeometricError,
+                refine : 'ADD',
+                children : [
+                    {
+                        boundingVolume : {
+                            region : llRegion
+                        },
+                        geometricError : 0.0,
+                        content : {
+                            uri : 'll' + ext
+                        }
+                    },
+                    {
+                        boundingVolume : {
+                            region : lrRegion
+                        },
+                        geometricError : 0.0,
+                        content : {
+                            uri : 'lr' + ext
+                        }
+                    },
+                    {
+                        boundingVolume : {
+                            region : urRegion
+                        },
+                        geometricError : 0.0,
+                        content : {
+                            uri : 'ur' + ext
+                        }
+                    },
+                    {
+                        boundingVolume : {
+                            region : ulRegion
+                        },
+                        geometricError : 0.0,
+                        content : {
+                            uri : 'ul' + ext
+                        }
+                    }
+                ]
+            }
+        };
+
+        const fullPath = path.join(rootDir, 'TilesetEmptyRoot');
+        await writeTileset(fullPath, tilesetJson as any, args);
+        for (let i=0; i < gltfs.length; ++i) {
+            const gltf = gltfs[i];
+            const tileFilename = tileNames[i] + ext;
+            await writeTile(fullPath, tileFilename, gltf, args);
+        }
+    }
 
     export async function createTilesetOfTilesets(args: GeneratorArgs) {}
 
