493892ee8f08c71440910307f7618cbdf0e25af5
Marco Hutter <javagl@javagl.de>
2022-12-08 15:18:08 +0100

    Removed unused classes

--- src/metadata/MetadataTableModel.ts
+++
@@ -1,9 +0,0 @@
-import { MetadataClass } from "../structure/Metadata/MetadataClass";
-import { MetadataEntityModel } from "./MetadataEntityModel";
-
-export interface MetadataTableModel {
-  get class(): MetadataClass;
-  get count(): number;
-
-  getEntity(index: number): MetadataEntityModel;
-}
